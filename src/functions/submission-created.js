9cc34970ef0979738377948bc96095f0eddd7a2a
Jay Delisle <j_delisle@my.uri.edu>
2020-09-11 22:04:32 -0400

    Added buttondown email subscription form to contact

---
+++ src/functions/submission-created.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const fetch = require('node-fetch')
+const {
+    EMAIL_TOKEN
+} = process.env.BUTTONDOWN_API_KEY
+exports.handler = async event => {
+    const email = JSON.parse(event.body).payload.email
+    console.log(`Recieved a submission: ${email}`)
+    return fetch('https://api.buttondown.email/v1/subscribers', {
+            method: 'POST',
+            headers: {
+                Authorization: `Token ${EMAIL_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: payload.email,
+                notes: payload.name
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(`Submitted to Buttondown:\n ${data}`)
+        })
+        .catch(error => ({
+            statusCode: 422,
+            body: String(error)
+        }))
+}
