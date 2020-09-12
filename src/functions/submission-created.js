014ef094adc14d161d2a9747f46e2e5cf0dd1bac
Jay Delisle <j_delisle@my.uri.edu>
2020-09-11 23:55:21 -0400

    removed content-type

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -1,26 +1,27 @@
 require("dotenv").config();
 const fetch = require("node-fetch");
-const { EMAIL_TOKEN } = process.env.BUTTONDOWN_API_KEY;
+const {
+    EMAIL_TOKEN
+} = process.env.BUTTONDOWN_API_KEY;
 exports.handler = async (event) => {
-  const payload = JSON.parse(event.body).payload;
-  console.log(`Recieved a submission: ${payload.email}`);
-  return fetch("https://api.buttondown.email/v1/subscribers", {
-    method: "POST",
-    headers: {
-      Authorization: `Token ${EMAIL_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: payload.email,
-      notes: payload.name,
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(`Submitted to Buttondown:\n ${data}`);
-    })
-    .catch((error) => ({
-      statusCode: 422,
-      body: String(error),
-    }));
+    const payload = JSON.parse(event.body).payload;
+    console.log(`Recieved a submission: ${payload.email}`);
+    return fetch("https://api.buttondown.email/v1/subscribers", {
+            method: "POST",
+            headers: {
+                Authorization: `Token ${EMAIL_TOKEN}`
+            },
+            body: JSON.stringify({
+                email: payload.email,
+                notes: payload.name,
+            }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(`Submitted to Buttondown:\n ${data}`);
+        })
+        .catch((error) => ({
+            statusCode: 422,
+            body: String(error),
+        }));
 };
