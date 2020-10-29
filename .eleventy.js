19b812bf90663eddede94c58075f4e52a1101263
Jay Delisle <j_delisle@my.uri.edu>
2020-10-28 20:04:26 -0400

    added rss feed (not styled) to site with eleventy rss plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss")
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/static");
+
+  eleventyConfig.addPlugin(pluginRss)
 
   eleventyConfig.addCollection("recentPosts", function (collection) {
     return collection.getFilteredByGlob("src/blogs/*.md").reverse();
